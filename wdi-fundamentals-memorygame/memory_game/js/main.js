001d7aed3182563314675c3c12288f549a08f111
malakhas <61279238+malakhas@users.noreply.github.com>
2020-03-04 16:15:55 +0300

    created game logic

--- wdi-fundamentals-memorygame/memory_game/js/main.js
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -28,20 +28,30 @@ function checkForMatch(){
 }
 }
 
-function flipCard(cardId){
+function flipCard(){
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank );
 cardsInPlay.push(cards[cardId].rank);
 
+this.setAttribute('src', cards[cardId].cardImage);
 if(cardsInPlay.length === 2){
 checkForMatch();
 }
 console.log(cards[cardId].cardImage)
 console.log(cards[cardId].suit)
+
 }
 
-
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+  }
+ }
+ createBoard();
 
 
 
