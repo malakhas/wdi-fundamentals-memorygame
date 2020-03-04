f1921adb68fa49fd85e8840113f77dc4656f7b16
malakhas <61279238+malakhas@users.noreply.github.com>
2020-03-04 14:31:33 +0300

    Add files via upload

---
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -0,0 +1,26 @@
+let cards = ["queen" , "queen" , "king" , "king"] ;
+let cardsInPlay = [] ;
+
+function checkForMatch(){
+	 if(cardsInPlay[0] === cardsInPlay[1] ){
+	console.log("You found a match!");
+} else {
+	console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId] );
+cardsInPlay.push(cardId);
+
+if(cardsInPlay.length === 2){
+checkForMatch();
+}
+
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
