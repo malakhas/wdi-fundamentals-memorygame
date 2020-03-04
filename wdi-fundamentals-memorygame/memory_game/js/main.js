970573f0f59f8087d9b4ed3bf7e80022b61a9cbf
malakhas <61279238+malakhas@users.noreply.github.com>
2020-03-04 15:15:29 +0300

    created game logic

--- wdi-fundamentals-memorygame/memory_game/js/main.js
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -1,4 +1,23 @@
-let cards = ["queen" , "queen" , "king" , "king"] ;
+let cards = [{
+	rank:"queen",
+	suit:"hearts"	,
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank:"queen"	,
+	suit:"diamonds"	,
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank:"king"	,
+	suit:"hearts"	,
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king"	,
+	suit:"diamonds"	,
+	cardImage:"images/king-of-diamonds.png"
+}] ;
 let cardsInPlay = [] ;
 
 function checkForMatch(){
@@ -10,14 +29,16 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-console.log("User flipped " + cards[cardId] );
-cardsInPlay.push(cardId);
+console.log("User flipped " + cards[cardId].rank );
+cardsInPlay.push(cards[cardId].rank);
 
 if(cardsInPlay.length === 2){
 checkForMatch();
 }
-
+console.log(cards[cardId].cardImage)
+console.log(cards[cardId].suit)
 }
+
 
 flipCard(0);
 flipCard(2);
